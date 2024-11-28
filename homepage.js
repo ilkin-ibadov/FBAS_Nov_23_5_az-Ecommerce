6cc8e1b2ac0a1f4dcd5de1520ca89a4af1ceb21e
Huseynli Tural Zeka <Husey_vw08@itstep.edu.az>
2024-11-28 16:05:47 +0400

    product items bug fix

--- homepage.js
+++ homepage.js
@@ -26,20 +26,18 @@ $(document).click(function (e) {
     }
 });
 
+let pageSize = 8;
+let currentPage = 1;
 let allProducts = [];
-let visibleProducts = 8;
 
 function createProductItems(data) {
-    container.html("");
-    const productsToShow = data.slice(0, visibleProducts);
-
-    productsToShow.forEach((item) => {
+    data.forEach((item) => {
         const productItem = `
             <div class="flex flex-col relative group">
                 <div class="relative">
                     <img class="w-[270px] h-[250px] object-scale-down border p-4 bg-[#F5F5F5] product-image" src="${item.gallery[0]}" alt="${item.title}">
                     <button class="add-to-basket absolute bottom-0 left-0 w-[270px] px-6 py-2 bg-black text-white rounded-none opacity-0 z-10 group-hover:opacity-100 transition-opacity duration-300">
-                        Add To Basket
+                        Add to Basket
                     </button>
                 </div>
                 <p class="mt-[18px]">${item.title}</p>
@@ -48,31 +46,29 @@ function createProductItems(data) {
         `;
         container.append(productItem);
     });
-
-    if (data.length > visibleProducts) {
-        showMoreButton.removeClass("hidden");
-    }
 }
 
-function getData() {
-    $.get("http://localhost:3000/api/products", (response) => {
+function getData(page = 1) {
+    $.get(`http://localhost:3000/api/products?page=${page}&pageSize=${pageSize}`, (response) => {
         allProducts = response.products;
         createProductItems(allProducts);
+        
+        if (allProducts.length === pageSize) {
+            showMoreButton.removeClass("hidden");
+        } else {
+            showMoreButton.addClass("hidden");
+        }
+    }).fail(function() {
+        alert('Veriler alınamadı.');
     });
 }
 
 showMoreButton.on("click", function () {
-    if (visibleProducts === 8) {
-        visibleProducts = allProducts.length;
-        showMoreButton.text("Show Less");
-    } else {
-        visibleProducts = 8;
-        showMoreButton.text("Show More");
-    }
-    createProductItems(allProducts);
+    currentPage++;
+    getData(currentPage);
 });
 
-getData();
+getData(currentPage);
 
 
 search.on("input", function (e) {
